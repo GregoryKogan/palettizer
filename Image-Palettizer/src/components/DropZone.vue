c0cc73107549814eba46732e587f4c6d33048664
GregoryKogan <grisha.koganovskiy@gmail.com>
2021-10-03 20:36:13 +0300

    Dropzone component completed

---
+++ Image-Palettizer/src/components/DropZone.vue
@@ -0,0 +1,71 @@
+<template>
+    <div 
+    @dragenter.prevent="toggleActive" 
+    @dragleave.prevent="toggleActive"
+    @dragover.prevent
+    @drop.prevent="toggleActive"
+    :class="{ 'active-dropzone': active }"
+    class="dropzone">
+        <span>Drag and Drop File</span>
+        <span>OR</span>
+        <label for="dropzoneFile">Select File</label>
+        <input type="file" id="dropzoneFile" class="dropzoneFile">
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    name: "DropZone",
+    setup(){
+        const active = ref(false);
+
+        const toggleActive = () => {
+            active.value = !active.value;
+        }
+
+        return { active, toggleActive };
+    }
+}
+</script>
+
+<style>
+    .dropzone {
+        width: 40%;
+        height: 30%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        row-gap: 16px;
+        border: 2px dashed #bd93f9;
+        background-color: #44475a;
+        color: #f8f8f2;
+        transition: .3 ease all;
+        border-radius: 10px;
+    }
+
+    .dropzone label {
+        padding: 8px 12px;
+        color: #f8f8f2;
+        background-color: #bd93f9;
+        transition: 0.3 ease all;
+        border-radius: 10px;
+    }
+
+    .dropzone input {
+        display: none;
+    }
+
+    .active-dropzone {
+        background-color: #bd93f9;
+        border-color: #44475a;
+        color: #44475a;
+    }
+
+    .active-dropzone label {
+        background-color: #44475a;
+        color: #f8f8f2;
+    }
+</style>
---
+++ Image-Palettizer/src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="home">
+        <h1>Image Palettizer</h1>
+        <DropZone v-if="!dropzoneFile" @drop.prevent="drop" @change="selectedFile"/>
+        <span class="file-info">File: {{dropzoneFile.name}}</span>
+    </div>
+</template>
+
+<script>
+import DropZone from '../components/DropZone.vue';
+import { ref } from 'vue';
+
+export default {
+    name: "Home",
+    components: {
+        DropZone,
+    },
+    setup(){
+        let dropzoneFile = ref("");
+
+        const drop = (event) => {
+            dropzoneFile.value = event.dataTransfer.files[0]
+        }
+
+        const selectedFile = () => {
+            dropzoneFile.value = document.querySelector(".dropzoneFile").files[0]
+        }
+
+        return { dropzoneFile, drop, selectedFile };
+    },
+}
+</script>
+
+<style>
+    .home {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: #282a36;
+        color: #f8f8f2;
+    }
+
+    .home h1 {
+        font-size: 40px;
+        margin-bottom: 32px;
+    }
+
+    .home .file-info {
+        margin-top: 32px;
+    }
+</style>
