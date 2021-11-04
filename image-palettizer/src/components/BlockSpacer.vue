f0d748addb37404a5ae9d151e48fa7605f0d764e
GregoryKogan <grisha.koganovskiy@gmail.com>
2021-11-04 14:20:24 +0300

    Block spacers refactored

--- image-palettizer/src/components/BlockSpacer.vue
+++ image-palettizer/src/components/BlockSpacer.vue
@@ -1,40 +1,48 @@
 <template>
-    <div class="block-spacer" :class="'spacer-' + type"></div>
+    <img class="block-spacer" :class="'spacer-' + type" :src="sourceImg">
 </template>
 
 <script>
 export default {
     name: 'BlockSpacer',
     props: ['type'],
+    data: () => ({
+      sourceImg: null,
+    }),
+    created(){
+      console.log(this.type)
+      if (this.type == 'bdl')
+        this.sourceImg = require('../assets/graphics/bottom-dark-light.svg')
+      else if (this.type == 'bld')
+        this.sourceImg = require('../assets/graphics/bottom-light-dark.svg')
+      else if (this.type == 'tdl')
+        this.sourceImg = require('../assets/graphics/top-dark-light.svg')
+      else if (this.type == 'tld')
+        this.sourceImg = require('../assets/graphics/top-light-dark.svg')
+      else if (this.type == 'tdl-thin')
+        this.sourceImg = require('../assets/graphics/top-dark-light-thin.svg')
+    },
 }
 </script>
 
 <style>
   .block-spacer{
     width: 100vw;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
   }
 
   .spacer-bdl{
     height: calc(100vw / 960 * 250);
-    background-image: url('../assets/graphics/bottom-dark-light.svg');
   }
   .spacer-bld{
     height: calc(100vw / 960 * 250);
-    background-image: url('../assets/graphics/bottom-light-dark.svg');
   }
   .spacer-tdl{
     height: calc(100vw / 960 * 250);
-    background-image: url('../assets/graphics/top-dark-light.svg');
   }
   .spacer-tdl-thin{
     height: calc(100vw / 960 * 100);
-    background-image: url('../assets/graphics/top-dark-light-thin.svg');
   }
   .spacer-tld{
     height: calc(100vw / 960 * 250);
-    background-image: url('../assets/graphics/top-light-dark.svg');
   }
 </style>
