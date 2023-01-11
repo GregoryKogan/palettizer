bd15e1d1683b44dd2e3db22462e73c7a2f1b3b81
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-01-12 01:46:52 +0300

    deploy scipt upgraded

--- image-palettizer/deploy.sh
+++ image-palettizer/deploy.sh
@@ -10,6 +10,8 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+cp index.html 404.html
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
@@ -21,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GregoryKogan/Image-Palettizer.git master:gh-pages
+git push -f git@github.com:GregoryKogan/Image-Palettizer.git main:gh-pages
 
 cd -
